1218ef08237f345237d81bf345402a5aafe35968
jreque21 <johnnyreque@gmail.com>
2026-09-21 23:51:57 -0500

    listo

--- public_html/vista/salir.php
+++ public_html/vista/salir.php
@@ -5,8 +5,7 @@ require_once("../modelo/class_usuario.php");
 $user_obj = new usuario();
 $data = $user_obj->f_usuario_logout();
 
-// Redirección absoluta: garantiza volver al login sin importar si esta página
-// fue alcanzada desde /vista/panel_admin.php o desde la raíz (index.php).
-header('Location: /vista/login_admin.php');
+// Redirección absoluta a la raíz (index.php), que muestra el formulario de login.
+header('Location: /');
 exit;
 ?>
